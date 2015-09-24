eca1346f1f74d7b5b62c794765e9cd7b80215106
Nathaniel Hercher <nhercher@gmail.com>
2015-09-23 17:21:49 -0700

    modified:   herbstluftwm/startup.sh

--- herbstluftwm/startup.sh
+++ herbstluftwm/startup.sh
@@ -2,5 +2,5 @@
 killall compton &
 (sleep 2s && compton) &
 killall trayer &
-(sleep 2s && trayer --edge bottom --align left --widthtype pixel --width 50 --height 15 --transparent true --alpha 255 --tint 0x333333 --expand true) &
+(sleep 2s && trayer --edge bottom --align center --widthtype pixel --width 50 --height 15 --transparent true --alpha 255 --tint 0x333333 --expand true) &
 feh --bg-fill ~/.wallpapers/9319373740_02243839ce_o\(2\).jpg &
