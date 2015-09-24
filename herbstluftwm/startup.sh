4387d3047e86a363e41331da5d1f9bd91218d798
Nathaniel Hercher <nhercher@gmail.com>
2015-09-24 14:40:23 -0700

    made comments in files for trayer and padding

--- herbstluftwm/startup.sh
+++ herbstluftwm/startup.sh
@@ -2,5 +2,8 @@
 killall compton &
 (sleep 2s && compton) &
 killall trayer &
-(sleep 2s && trayer --edge bottom --align right --widthtype pixel --width 200 --height 16 --transparent true --alpha 0 --tint 0x333333 --expand true) &
+## trayer with dzenpanel on bottom
+#(sleep 2s && trayer --edge bottom --align right --widthtype pixel --width 200 --height 16 --transparent true --alpha 0 --tint 0x333333 --expand false) &
+## trayer with no dzenpanel
+(sleeps 2s && trayer --edge bottom --align center --widthype pixel --width 50 --height 15 --transparent true --alpha 255 --tint 0x333333 --expand true) &
 feh --bg-fill ~/.wallpapers/9319373740_02243839ce_o\(2\).jpg &
